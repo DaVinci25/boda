48479e2c1566a8ecc977fc28b9a45d7d49777e84
DaVinci25 <said25022004@gmail.com>
2026-01-14 16:59:06 +0100

    arreglar el boton de cofirmar asistecia1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { RsvpComponent } from './components/rsvp/rsvp.component';
 import { DetailsComponent } from './components/details/details.component';
-import { StoryComponent } from './components/story/story.component';
+//import { StoryComponent } from './components/story/story.component';
 import { GiftsComponent } from './components/gifts/gifts.component';
 import { FaqComponent } from './components/faq/faq.component';
 
@@ -10,7 +10,7 @@ export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'rsvp', component: RsvpComponent },
   { path: 'detalles', component: DetailsComponent },
-  { path: 'historia', component: StoryComponent },
+ // { path: 'historia', component: StoryComponent },
   { path: 'regalos', component: GiftsComponent },
   { path: 'faq', component: FaqComponent },
   { path: '**', redirectTo: '' }
